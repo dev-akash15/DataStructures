f9125d567b7cc3d90327a3bd2f5cda2c40f7d9f4
dev-akash15 <112691487+dev-akash15@users.noreply.github.com>
2022-10-06 19:26:43 +0530

    Add files via upload

---
+++ Recursion/BinarySearchAsc.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class BinarySearchAsc{
+
+	public static void main(String[] arr){
+		
+		Scanner sc = new Scanner(System.in);	
+	
+		int[] array = {1,2,4,5,7,9,10};
+
+		for(int i=0;i<array.length;i++)
+		{
+			System.out.print(array[i]+" ");
+		}
+		System.out.println();
+		System.out.print("Enter the element from above to search");
+		System.out.println();
+		
+		int element = sc.nextInt();
+		
+		int index = binarySearch(array,element);
+		if(index==-1)
+		{
+			System.out.println("Element not found");
+		}
+		else
+		{
+			System.out.println("Element found at index : "+index);
+		}
+
+	}
+
+	public static int binarySearch(int[] arr,int element)
+	{
+		int low = 0;
+		int high = arr.length;
+		
+		while(low<=high)
+		{
+			int mid = low +((high-low)/2);
+			if(arr[mid]==element)
+			{
+				return mid;
+			}
+			else if(arr[mid]<element)
+			{
+				low = mid+1;
+			}
+			else
+			{
+				high = mid-1;
+			}
+		}
+		return -1;
+	}
+
+}
